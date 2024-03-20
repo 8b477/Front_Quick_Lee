7f68d81b86d21938944ae1acf2ab4b0eb8705afb
8b477 <jonathan_buchet@outlook.fr>
2024-03-20 16:58:20 +0100

    initialization of the basic project structure

---
+++ Quick_Lee/Components/Models/DicoWords.cs
@@ -0,0 +1,8 @@
+﻿namespace Quick_Lee.Components.Models
+{
+    public class DicoWords(string wordToAdd, string wordDefinition)
+    {
+        public string WordToAdd = wordToAdd;
+        public string WordDefinition = wordDefinition;
+    }
+}
---
+++ Quick_Lee/Components/Pages/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿using Quick_Lee.Components.Models;
+
+namespace Quick_Lee.Components.Pages
+{
+    public partial class Home
+    {
+
+        readonly List<DicoWords> WordsList = [];
+
+        string WordToAdd = string.Empty;
+        string WordDefinition = string.Empty;
+
+
+        public void AddToList()
+        {
+            DicoWords wordDicoToAdd = new(this.WordToAdd, this.WordDefinition);
+
+            WordsList.Add(wordDicoToAdd);
+        }
+ 
+       
+    }
+}
